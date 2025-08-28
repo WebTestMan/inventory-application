f4a7640c76af9d2950adbd44a0ecc2a13432674b
Mark Guhathakurta-Halliday <133343189+WebTestMan@users.noreply.github.com>
2025-08-28 14:31:59 +0100

    cleaned up app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 require("dotenv").config();
-const customRouter = require("./routes/router");
+const inventoryRouter = require("./routes/router");
 const app = express();
 const path = require("node:path");
 const assetsPath = path.join(__dirname, "public");
@@ -8,13 +8,11 @@ const assetsPath = path.join(__dirname, "public");
 app.use(express.static(assetsPath));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
-app.use("/", customRouter);
+
+app.use("/", inventoryRouter);
 
 const PORT = 3000;
 app.listen(PORT, (error) => {
-  // This is important!
-  // Without this, any startup errors will silently fail
-  // instead of giving you a helpful error message.
   if (error) {
     throw error;
   }
